ff0b0ca25f64b9fcfd0a1bc4894585b453815db5
emreerkmen <emre.erkmen@hotmail.com>
2020-04-21 23:05:42 +0300

    mid of lesson 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play('note1.wav');
+                    },
+                    child: Text(nouns.last),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      final player = AudioCache();
+                      player.play('note2.wav');
+                    },
+                    child: Text(nouns.first),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
